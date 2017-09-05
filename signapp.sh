ed97a559672e4d2df67ca311730e230c28559c64
Rishon73 <sshiff@gmail.com>
2017-09-05 14:07:28 -0400

    Support new(er) AOS app

--- signapp.sh
+++ signapp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-#### Change log ####
+######### Change log #########
+# 8/2/2014
+# adding $APP_NAME variable, without the .ipa extention
+# removed '-v' flag (verbose)
+#
+##############################
 
 VERSION="$1"
 BASE=/Users/shahar/OneDrive\ -\ Hewlett\ Packard\ Enterprise/4.\ Work/1.Mobile\ Center/
@@ -8,28 +13,33 @@ cd "${BASE}"
 echo directory is now: "$(pwd)"
 
 NEW_FOLDER=$(date +%y-%m-%d-%H%M%S)
-IPA_FOLDER=Mobile\ Apps/AOS/May\ 25\ 2017\ -\ v1.1.1
+IPA_FOLDER=Mobile\ Apps/AOS/July\ 2017\ -\ v1.1.2
 PATH2="${IPA_FOLDER}/$NEW_FOLDER"
+APP_NAME=Advantage+demo+1_1_2
 
 mkdir "${PATH2}"
 echo "${PATH2}" was created...
 
-echo About to run "--$VERSION/HPMCEnabler/HPMCEnabler--" on "--$IPA_FOLDER/Advantage\ Shopping.ipa--" command now...
+echo About to run "--$VERSION/HPMCEnabler/HPMCEnabler--" on "--$IPA_FOLDER/$APP_NAME.ipa--" command now...
 
-$VERSION/HPMCEnabler/HPMCEnabler "${IPA_FOLDER}"/Advantage\ Shopping.ipa \
+$VERSION/HPMCEnabler/HPMCEnabler "${IPA_FOLDER}"/$APP_NAME.ipa \
 -i $VERSION/HPMCEnabler/HPMobileCenter.dylib \
 -j $VERSION/HPMCEnabler/HybridJS \
 -c "iPhone Developer: Shahar Shiff (S524ZZQSE5)" \
 -p MC_Provision_Profile.mobileprovision \
--d "${PATH2}" \
--v
+-d "${PATH2}"
 
-echo Renaming ${PATH2}/Advantage Shopping-HPMCEnabled.ipa to ${PATH2}/Advantage Shopping-HPMCEnabled-${VERSION}.ipa
+echo Renaming ${PATH2}/${APP_NAME}-HPMCEnabled.ipa to ${PATH2}/${APP_NAME}-HPMCEnabled-${VERSION}.ipa
 
-echo Renaming app... $PATH2
 cd "$PATH2"
 echo directory is now: "$(pwd)"
 
-mv Advantage\ Shopping-HPMCEnabled.ipa Advantage\ Shopping-HPMCEnabled-${VERSION}.ipa
+#mv Advantage\ Shopping-HPMCEnabled.ipa Advantage\ Shopping-HPMCEnabled-${VERSION}.ipa
+mv ${APP_NAME}-HPMCEnabled.ipa ${APP_NAME}-HPMCEnabled-${VERSION}.ipa
+
+echo copy to "Shared MC Content/Signed AOS" folder
+echo "${BASE}"
+/bin/cp ${APP_NAME}-HPMCEnabled-${VERSION}.ipa "${BASE}SharedMCContent/SignedAOS/"
+echo $?
 
 echo Done!!!
