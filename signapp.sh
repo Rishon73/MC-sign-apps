56d944668c92df0b09a6d25938cce2418fbb44b7
Rishon73 <sshiff@gmail.com>
2017-09-18 22:13:18 -0400

    1. Added iPad path as argument. 2. Changed path structure 3. mv artifact instead mv and cp

--- signapp.sh
+++ signapp.sh
@@ -4,42 +4,59 @@
 # 8/2/2014
 # adding $APP_NAME variable, without the .ipa extention
 # removed '-v' flag (verbose)
-#
+
+# 8/18/2017
+# Added second arg (APP_FULL_PATH) and split it to folder, file name and file name w/o extention
+# Also, moging the sign artifact file directly to SharedMCContent/SignedAOS folder (not renaming and copying anymore)
 ##############################
 
 VERSION="$1"
+APP_FULL_PATH="$2"
 BASE=/Users/shahar/OneDrive\ -\ Hewlett\ Packard\ Enterprise/4.Work/1.Mobile\ Center/
 cd "${BASE}"
 echo directory is now: "$(pwd)"
 
 NEW_FOLDER=$(date +%y-%m-%d-%H%M%S)
-IPA_FOLDER=Mobile\ Apps/AOS/July\ 2017\ -\ v1.1.2
-PATH2="${IPA_FOLDER}/$NEW_FOLDER"
-APP_NAME=Advantage+demo+1_1_2
+#IPA_FOLDER=Mobile\ Apps/AOS/July\ 2017\ -\ v1.1.2
+IPA_FOLDER=$(dirname "${APP_FULL_PATH}")
+#PATH2="${IPA_FOLDER}/$NEW_FOLDER"
+#APP_NAME=Advantage+demo+1_1_2
+APP_NAME=$(basename "${APP_FULL_PATH}")
+APP_NAME_NO_EXT="${APP_NAME%.*}"
 
-mkdir "${PATH2}"
-echo "${PATH2}" was created...
+echo "========= Print variables ============="
+echo "VERSION: "$VERSION
+echo "APP_FULL_PATH: "$APP_FULL_PATH
+echo "BASE: "$BASE
+echo "APP_NAME: "$APP_NAME
+echo "APP_NAME_NO_EXT: "$APP_NAME_NO_EXT
+echo "IPA_FOLDER: "$IPA_FOLDER
+echo "NEW_FOLDER: "$NEW_FOLDER
+echo "========================================"
+#echo "PATH2: "$PATH2
 
-echo About to run "--$VERSION/HPMCEnabler/HPMCEnabler--" on "--$IPA_FOLDER/$APP_NAME.ipa--" command now...
+#mkdir "${PATH2}"
+#echo "${PATH2}" was created...
 
-$VERSION/HPMCEnabler/HPMCEnabler "${IPA_FOLDER}"/$APP_NAME.ipa \
+#echo About to run "--$VERSION/HPMCEnabler/HPMCEnabler--" on "--$IPA_FOLDER/$APP_NAME.ipa--" command now...
+echo About to run "--$VERSION/HPMCEnabler/HPMCEnabler--" on "--$IPA_FOLDER/$APP_NAME" command now...
+
+#$VERSION/HPMCEnabler/HPMCEnabler "${IPA_FOLDER}"/$APP_NAME.ipa \
+$VERSION/HPMCEnabler/HPMCEnabler "${APP_FULL_PATH}" \
 -i $VERSION/HPMCEnabler/HPMobileCenter.dylib \
 -j $VERSION/HPMCEnabler/HybridJS \
 -c "iPhone Developer: Shahar Shiff (S524ZZQSE5)" \
 -p MC_Provision_Profile.mobileprovision \
--d "${PATH2}"
+-d "${IPA_FOLDER}"
 
-echo Renaming ${PATH2}/${APP_NAME}-HPMCEnabled.ipa to ${PATH2}/${APP_NAME}-HPMCEnabled-${VERSION}.ipa
-
-cd "$PATH2"
+cd "$IPA_FOLDER"
 echo directory is now: "$(pwd)"
 
-#mv Advantage\ Shopping-HPMCEnabled.ipa Advantage\ Shopping-HPMCEnabled-${VERSION}.ipa
-mv ${APP_NAME}-HPMCEnabled.ipa ${APP_NAME}-HPMCEnabled-${VERSION}.ipa
+echo Moving ${IPA_FOLDER}/${APP_NAME_NO_EXT}-HPMCEnabled.ipa to $"${BASE}SharedMCContent/SignedAOS/"${APP_NAME_NO_EXT}-HPMCEnabled-${VERSION}.ipa
 
-echo copy to "Shared MC Content/Signed AOS" folder
-echo "${BASE}"
-/bin/cp ${APP_NAME}-HPMCEnabled-${VERSION}.ipa "${BASE}SharedMCContent/SignedAOS/"
+#mv Advantage\ Shopping-HPMCEnabled.ipa Advantage\ Shopping-HPMCEnabled-${VERSION}.ipa
+#mv ${APP_NAME_NO_EXT}-HPMCEnabled.ipa ${APP_NAME_NO_EXT}-HPMCEnabled-${VERSION}.ipa
+mv ${APP_NAME_NO_EXT}-HPMCEnabled.ipa "${BASE}SharedMCContent/SignedAOS/"${APP_NAME_NO_EXT}-HPMCEnabled-${VERSION}.ipa
 echo $?
 
 echo Done!!!
